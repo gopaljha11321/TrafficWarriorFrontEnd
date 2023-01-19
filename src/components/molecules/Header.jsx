0a0f82f2a7755b577145777fe874fae2620c4360
Gopal Jha <82890881+gopaljha11321@users.noreply.github.com>
2023-01-19 09:49:55 +0530

    profile ui change

--- src/components/molecules/Header.jsx
+++ src/components/molecules/Header.jsx
@@ -33,11 +33,12 @@ const Header = (props) => {
   return ( 
     <>
     {profile?<>
-    <div id="container1">
-      <div className="image" align="center"style={{fontFamily:"arial,sans-serif",fontWeight:"700",color:"black",width:"100%",height:"100%"}}><img src="user_icon.webp" width="200px" style={{clipPath:"circle(40%)"}} alt="user_image"/>
-      <div style={{fontWeight:"700",color:"black"}}>{props.name}</div>
-      <div style={{borderBottom:"1px  solid grey", paddingBottom:"50px",fontWeight:"500",color:"grey"}}>{props.email}</div>
-      <div className="button" style={{marginTop:"25px"}}><button type="submit" style={{border:"1px grey solid", borderRadius:"10px", width:"100px", height:"40px",fontWeight:"500",cursor:"pointer"}} className="btn1" onClick={signOut}>Sign out</button></div>
+    <div id="container1" >
+      <div className="image" align="center"style={{fontFamily:"arial,sans-serif",width:"100%",height:"100%"}}><img src="user_icon.webp" width="200px" style={{clipPath:"circle(40%)"}} alt="user_image"/>
+      <div id ="edit_image"><img src="edit.png" style={{}} width="42px"/></div>
+      <div style={{fontWeight:"700",color:"black",position:"relative",top:"-70px"}}>{props.name}</div>
+      <div style={{borderBottom:"1px  solid grey",position:"relative",top:"-70px", paddingBottom:"30px",fontWeight:"500",color:"grey"}}>{props.email}</div>
+      <div className="button" style={{marginTop:"25px",position:"relative",top:"-75px"}}><button type="submit" style={{border:"1px grey solid", borderRadius:"10px", width:"100px", height:"40px",fontWeight:"500",cursor:"pointer"}} className="btn1" onClick={signOut}>Sign out</button></div>
       </div>
       
     </div>
